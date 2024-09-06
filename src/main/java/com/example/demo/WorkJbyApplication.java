e295d1c717de3b64240748912c7bedf553de7467
jby9725 <jby9725@gmail.com>
2024-09-06 09:28:13 +0900

    project setting

---
+++ src/main/java/com/example/demo/WorkJbyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkJbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkJbyApplication.class, args);
+	}
+
+}
